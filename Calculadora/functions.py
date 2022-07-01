1d42ab12932f2d8e9d562eccf366c102ef70a71f
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-07-01 01:03:33 -0300

    Funçoes basicas prontas

--- Calculadora/functions.py
+++ Calculadora/functions.py
@@ -4,3 +4,15 @@ from re import A
 def soma(a,a1):
     somar = a + a1
     return somar
+
+def subtra(a,a1):
+    subtrair = a - a1
+    return subtrair
+
+def multi(a,a1):
+    multiplicar = a * a1
+    return multiplicar
+
+def dividir(a,a1):
+    dividir = a / a1
+    return dividir
--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,6 +1,25 @@
 import functions
 
-valor1 = float(input('Digite o 1 valor:\n'))
-valor2 = float(input('Digite o 2 valor:\n'))
-resultado = functions.soma(valor1,valor2)
-print(f'O resultado foi:{resultado}')
+opcao = str(input('Escolha qual das operaçoes voce quer fazer:\n A) Soma\n B) Subtracao\n C) Multiplicacao\n D) Divisao\n Digite a opçao desejada:\n -->'))
+if opcao == 'A' or opcao == 'a':
+    valor1 = float(input('Digite o primeiro valor desejado:\n -->'))
+    valor2 = float(input('Digite o segundo valor desejado:\n -->'))
+    resultado = functions.soma(valor1, valor2)
+    print(f'O resultado da soma {valor1} + {valor2} é igual a: {resultado}')
+elif opcao == 'B' or opcao == 'b':
+    valor1 = float(input('Digite o primeiro valor desejado:\n -->'))
+    valor2 = float(input('Digite o segundo valor desejado:\n -->'))
+    resultado = functions.subtra(valor1, valor2)
+    print(f'O resultado da subtraçao {valor1} - {valor2} é igual a: {resultado}')
+elif opcao == 'C' or opcao == 'c':
+    valor1 = float(input('Digite o primeiro valor desejado:\n -->'))
+    valor2 = float(input('Digite o segundo valor desejado:\n -->'))
+    resultado = functions.multi(valor1, valor2)
+    print(f'O resultado da multiplicaçao {valor1} x {valor2} é igual a: {resultado}')
+elif opcao == 'D' or opcao == 'd':
+    valor1 = float(input('Digite o primeiro valor desejado:\n -->'))
+    valor2 = float(input('Digite o segundo valor desejado:\n -->'))
+    resultado = functions.dividir(valor1, valor2)
+    print(f'O resultado da divisao {valor1} / {valor2} é igual a: {resultado}')
+else:
+    print('error...')
