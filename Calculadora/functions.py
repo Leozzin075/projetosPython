9735574715aa60ab68bb4209911cc0ddc84fa41a
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-07-01 00:25:54 -0300

    Funçao de soma feita...

---
+++ Calculadora/functions.py
@@ -0,0 +1,6 @@
+from re import A
+
+
+def soma(a,a1):
+    somar = a + a1
+    return somar
---
+++ Calculadora/main.py
@@ -0,0 +1,6 @@
+import functions
+
+valor1 = float(input('Digite o 1 valor:\n'))
+valor2 = float(input('Digite o 2 valor:\n'))
+resultado = functions.soma(valor1,valor2)
+print(f'O resultado foi:{resultado}')
